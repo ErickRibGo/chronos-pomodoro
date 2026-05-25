e8e1f53347850130e498d41556219ed3cff9db28
Erick Ribeiro <erickribgo200212@gmail.com>
2026-05-24 21:37:01 -0300

    Novas atualizações

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { Menu } from './components/Menu';
 
 import './styles/theme.css';
 import './styles/global.css';
+import { CountDown } from './components/CountDown';
 
 
 export function App() {
@@ -15,6 +16,10 @@ export function App() {
 
       <Container>
           <Menu />
+      </Container>
+
+      <Container>
+          <CountDown />
       </Container>
     </> 
 
